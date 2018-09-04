611e5ef71e695d07d210a34d4ed32f05db4500a2
dossey615 <dossey615@gmail.com>
2018-09-04 11:14:27 +0900

    スピナーの追加

--- app/src/main/java/com/example/dosshi/isolationassist/PartsDescriptionActivity.java
+++ app/src/main/java/com/example/dosshi/isolationassist/PartsDescriptionActivity.java
@@ -3,9 +3,8 @@ package com.example.dosshi.isolationassist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-//import android.view.View;
-//import android.widget.Button;
+import android.widget.*;
+import android.view.*;
 
 public class PartsDescriptionActivity extends AppCompatActivity {
 
@@ -17,5 +16,22 @@ public class PartsDescriptionActivity extends AppCompatActivity {
         String data = intent.getStringExtra(SelectPartsActivity.PARTS_DATA);
         TextView textView = findViewById(R.id.SelectedParts);
         textView.setText(data);
+        initSpinners();
     }
+
+    private void initSpinners() {
+        Spinner spinner1 = (Spinner)findViewById(R.id.Spinner01);
+        String[] labels = getResources().getStringArray(R.array.time_array);
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, labels);
+        spinner1.setAdapter(adapter);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
+
+    public void onClick_Button1(View view){
+        Spinner spinner1 = (Spinner)findViewById(R.id.Spinner01);
+        String str = spinner1.getSelectedItem().toString();
+    }
+
 }
